a37b1b099a2b8e0b329c120fda0dc49f165a6a7a
Noemi Delgado <noemi.delgado@leanmind.es>
2024-10-04 15:56:29 +0100

    Add gradle support

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+    @Test
+    public void testDummy() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testDummy2() {
+        Assert.assertTrue(true);
+    }
+}
