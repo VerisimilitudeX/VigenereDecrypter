8dfddc98003d9c122796c17949fbcc84fed29b12
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-10-11 13:05:21 +0200

    Add `cipherPlainTextWithKey` test

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -9,4 +9,12 @@ public class CaesarCipherTest {
         assertThat(caesarCipher.encrypt("defend the east wall of the castle"))
                 .isEqualTo("efgfoe uif fbtu xbmm pg uif dbtumf");
     }
+
+    @Test
+    public void cipherPlainTextWithKey() {
+        var caesarCipher = new CaesarCipher(4);
+
+        assertThat(caesarCipher.encrypt("efgfoe uif fbtu xbmm pg uif dbtumf"))
+                .isEqualTo("ijkjsi ymj jfxy bfqq tk ymj hfxyqj");
+    }
 }
